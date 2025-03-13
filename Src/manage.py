772ffebc1d5ba5cc8a52b1be0f5e086d6a8e5140
DandyWildan95 <wildandandy72@gmail.com>
2025-03-14 04:14:12 +0700

    Fix manage.py configuration for migrations

--- Src/manage.py
+++ Src/manage.py
@@ -1,11 +1,12 @@
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask import Flask
+from flask_migrate import Migrate
+from flask_script import Manager  # Make sure this is installed
 from src.app import create_app
 from src.models.user import db
 
-app = create_app()
-migrate = Migrate(app, db)
-manager = Manager(app)
+application = create_app()
+migrate = Migrate(application, db)
+manager = Manager(application)  # Use application instead of app
 
 manager.add_command('db', MigrateCommand)
 
